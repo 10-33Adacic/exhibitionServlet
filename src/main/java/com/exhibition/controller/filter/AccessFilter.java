38e95b513c5294561a66719f95fba41ba1a46a6d
Me <m.sadovskiy87@gmail.com>
2020-04-14 18:10:12 +0300

    add filters

---
+++ src/main/java/com/exhibition/controller/filter/AccessFilter.java
@@ -0,0 +1,46 @@
+package com.exhibition.controller.filter;
+
+
+import com.exhibition.model.entity.Role;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+//import static com.exhibition.controller.util.Constants.URL_INDEX;
+
+public class AccessFilter extends AbstractFilter {
+
+    @Override
+    public void doFilter(HttpServletRequest request,
+                         HttpServletResponse response,
+                         FilterChain chain
+    ) throws IOException, ServletException {
+
+        String path = request.getRequestURI();
+
+        if (path.contains("super_admin")) {
+            if (request.getSession().getAttribute("role") == Role.SUPER_ADMIN) {
+                chain.doFilter(request, response);
+            } else {
+                response.sendRedirect("/exhibition/index");
+            }
+        } else if (path.contains("admin")) {
+            if (request.getSession().getAttribute("role") == Role.ADMIN) {
+                chain.doFilter(request, response);
+            } else {
+                response.sendRedirect("/exhibition/index");
+            }
+        }  else if (path.contains("user")) {
+            if (request.getSession().getAttribute("role") == Role.ADMIN ||
+                    request.getSession().getAttribute("role") == Role.USER) {
+                chain.doFilter(request, response);
+            } else {
+                response.sendRedirect("/exhibition/index");
+            }
+        }  else {
+            chain.doFilter(request, response);
+        }
+    }
+}
---
+++ src/main/java/com/exhibition/controller/filter/EncodingFilter.java
@@ -0,0 +1,24 @@
+package com.exhibition.controller.filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class EncodingFilter extends AbstractFilter {
+
+    @Override
+    public void doFilter(HttpServletRequest request,
+                         HttpServletResponse response,
+                         FilterChain chain
+    ) throws IOException, ServletException {
+
+        request.setCharacterEncoding("UTF-8");
+
+        response.setCharacterEncoding("UTF-8");
+
+        response.setContentType("text/html;charset=UTF-8");
+
+        chain.doFilter(request,response);
+    }
+}
