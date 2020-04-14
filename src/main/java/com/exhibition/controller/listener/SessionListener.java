745c168b0f2ef957e0d399900137ecc3dd0e5942
Me <m.sadovskiy87@gmail.com>
2020-04-14 18:10:43 +0300

    add listener

---
+++ src/main/java/com/exhibition/controller/listener/SessionListener.java
@@ -0,0 +1,28 @@
+package com.exhibition.controller.listener;
+
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+import java.util.HashSet;
+
+public class SessionListener implements HttpSessionListener {
+
+    @Override
+    public void sessionCreated(HttpSessionEvent httpSessionEvent){}
+
+    @Override
+    public void sessionDestroyed(HttpSessionEvent httpSessionEvent) {
+        @SuppressWarnings("unchecked")
+        HashSet<String> loggedUsers = (HashSet<String>) httpSessionEvent
+                .getSession()
+                    .getServletContext()
+                        .getAttribute("loggedUsers");
+
+        String username = (String) httpSessionEvent
+                .getSession()
+                    .getAttribute("username");
+
+        loggedUsers.remove(username);
+
+        httpSessionEvent.getSession().setAttribute("loggedUsers", loggedUsers);
+    }
+}
